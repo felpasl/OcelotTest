f220bff1a593f6b6c72702cc75d5e1d1c4c8ae81
Felipe Lambert <felipe.lambert@outlook.com>
2018-12-21 16:32:56 -0200

    Correção de Nomes

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -7,18 +7,33 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace Api2Test
+namespace ApiTest
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
-        }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            IWebHostBuilder builder = new WebHostBuilder();
+
+            builder.ConfigureServices(s => {
+                s.AddSingleton(builder);
+            });
+
+            builder.UseKestrel()
+                .ConfigureLogging(logging => {
+                    logging.ClearProviders();
+                    logging.AddConsole();
+                })
+                .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseStartup<Startup>();
+
+            var host = builder.Build();
+
+            host.Run();
+        }
+        
     }
 }
--- Gateway/Startup.cs
+++ Gateway/Startup.cs
@@ -1,53 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using NJsonSchema;
-using NSwag.AspNetCore;
-using System.Reflection;
+using ConfigurationBuilder = Microsoft.Extensions.Configuration.ConfigurationBuilder;
 
-namespace Api2Test
+namespace ApiTest
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+            .AddJsonFile("ocelot.json")
+            .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
+        public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            services.AddSwaggerDocument(configure => {
-                configure.DefaultPropertyNameHandling = PropertyNameHandling.CamelCase;
-                configure.Title = "ApiTeste";
-              configure.DocumentProcessors.Add()
-            } );
+            services.AddOcelot(Configuration);
+            services.AddSwaggerForOcelot(Configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseMvc();
-            app.UseSwagger();
-            app.UseSwaggerUi3(configure => {
-                
-            });
+            app.UseSwaggerForOcelotUI(Configuration);
+            app.UseOcelot().Wait();
         }
     }
 }
